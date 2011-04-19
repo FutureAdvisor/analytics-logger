fab661d353e013eca0c8e5ae21a220512475eb2c
Jared McFarland <jared.online@gmail.com>
2011-04-18 20:05:16 -0700

    fixing typo

--- lib/analytics_logger.rb
+++ lib/analytics_logger.rb
@@ -1,3 +1,3 @@
 require 'active_record'
 
-require 'analytics_logger/analytics' unless defined?(AnaltyicsEvent).nil? or defined?(AnalyticsEventType).nil?
+require 'analytics_logger/analytics' unless defined?(AnalyticsEvent).nil? or defined?(AnalyticsEventType).nil?
